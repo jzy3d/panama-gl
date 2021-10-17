9898e49a629f1d9e1b138d0027db3a88d62ef903
Martin Pernollet <martin.pernollet@bioserenity.com>
2021-10-17 14:32:18 +0200

    clean

--- src/main/java/org/jzy3d/demos/teapot/TeapotDemoPanamaGL.java
+++ src/main/java/org/jzy3d/demos/teapot/TeapotDemoPanamaGL.java
@@ -2,6 +2,7 @@ package org.jzy3d.demos.teapot;
 
 import org.jzy3d.chart.Chart;
 import org.jzy3d.chart.factories.ChartFactory;
+import org.jzy3d.chart.factories.EmulGLChartFactory;
 import org.jzy3d.chart.factories.PanamaGLChartFactory;
 import org.jzy3d.colors.Color;
 import org.jzy3d.plot3d.rendering.canvas.Quality;
@@ -11,8 +12,8 @@ import org.jzy3d.utils.LoggerUtils;
 public class TeapotDemoPanamaGL {
 
   public static void main(String[] args) {
-    LoggerUtils.maximal();
-    
+    //LoggerUtils.maximal();
+
     Teapot teapot = new Teapot();
     teapot.setFaceDisplayed(true);
     teapot.setColor(Color.WHITE);
@@ -20,36 +21,32 @@ public class TeapotDemoPanamaGL {
     teapot.setWireframeDisplayed(false);
     teapot.setWireframeWidth(2);
     teapot.setReflectLight(true);
-    
+
     // ---------------------------------------------
     ChartFactory factory = new PanamaGLChartFactory();
-    
     //ChartFactory factory = new EmulGLChartFactory();
-    
+
     // Emulgl will show limitations
     // 1-wireframe and face do not mix cleanly (polygon offset fill)
     // 2-wireframe color tend to saturate (here in green)
-    
-    Quality q = Quality.Advanced(); 
+
+    Quality q = Quality.Advanced();
     q.setDepthActivated(true);
     q.setAlphaActivated(false);
     q.setAnimated(true);
     q.setHiDPIEnabled(false);
-    
+
     Chart chart = factory.newChart(q);
 
     chart.getView().setSquared(false);
     chart.getView().setBackgroundColor(Color.BLACK);
     chart.getView().getAxis().getLayout().setMainColor(Color.WHITE);
     chart.getView().setAxisDisplayed(true);
-    // ---------------------------------------------
 
     chart.add(teapot);
-    
-    //Light light = chart.addLightOnCamera();
+
     Light light = chart.addLight(chart.getView().getBounds().getCorners().getXmaxYmaxZmax());
-    //light.setRepresentationDisplayed(true);
-    
+
     // ---------------------------------------------
 
     // Manual HiDPI setting
